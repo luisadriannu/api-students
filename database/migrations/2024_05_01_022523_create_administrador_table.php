d3e7792862f52dfa7fc6dbd793d27ae7d681ac9a
Luis <luisadriannu13@gmail.com>
2024-05-02 11:04:14 -0600

    Primer commit

---
+++ database/migrations/2024_05_01_022523_create_administrador_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrador', function (Blueprint $table) {
+            $table->char('id_admin', 5)->primary();
+            $table->string('nom_admin', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrador');
+    }
+};
---
+++ database/migrations/2024_05_01_022556_create_matricula_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matricula', function (Blueprint $table) {
+            $table->char('id_matricula', 5)->primary();
+            $table->unsignedBigInteger('id_alumno');
+            $table->char('id_curso', 5);
+            $table->char('id_docente', 5);
+            $table->char('periodo', 7);
+            $table->char('mes_inicio', 20);
+            $table->date('fecha_inicio');
+            $table->foreign('id_alumno')->references('id_alumno')->on('alumno');
+            $table->foreign('id_curso')->references('id_curso')->on('curso');
+            $table->foreign('id_docente')->references('id_docente')->on('docente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matricula');
+    }
+};
